46fd64160ed97b70994dbc24fabea9c2f47d451d
Mauricio Ferrari <m10ferrari1200@gmail.com>
2026-05-24 13:53:15 -0300

    use auto help

--- src/aports.rs
+++ src/aports.rs
@@ -55,9 +55,14 @@ impl Aports {
             Arg::value(Some("-p"), "--profile", "profile", &mut profile),
         ];
 
-        parse_into_vars("aports", &mut rules, HELP_RULES, args)
+        if parse_into_vars("aports", &mut rules, HELP_RULES, args)
             .strict()
-            .require_args()?;
+            .require_args()?
+            .help_requested()
+        {
+            return Ok(());
+        }
+
         drop(rules);
 
         utils::check_rootfs_exists(rootfs_dir.clone())?;
